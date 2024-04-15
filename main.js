0e17a1a7f3c2501c6340b65e53ab9a4fef150e03
Jdmedina08 <jdmedina08@hotmail.es>
2024-04-15 18:31:35 -0500

    primer commit

--- main.js
+++ main.js
@@ -1,91 +1,90 @@
-function validateName(){
-    let name = document.getElementById("name").value;
+// class Nodo{
+//     constructor(valor){
+//         this.valor = valor;
+//         this.siguiente = null;
+//     }
+// }
+// class Pila {
+//     constructor(){
+//         this.tope = null;
+//     }
+//     agregarElemento(valor){
+//         let nuevoElemento = new Nodo(valor);
+//         nuevoElemento.siguiente = this.tope;
+//         this.tope = nuevoElemento;
+//     }
+
+
+//     agregarElemento (valor){
+//         let nuevo = new Nodo(valor);
+//         nuevo.siguiente = this.tope;
+//         this.tope = nuevo;
+//     }
+//     mostrarElementos(){
+//         let actual = this.tope;
+//         while(actual!=null){
+//             console.log(actual.valor);
+//             actual = actual.siguiente;
+//         }
+//     }    
+// }
+// function eliminar(){
+//     primero = primero.siguiente;
+// }
+// const pila = new Pila();
+// pila.agregarElemento(15);
+// pila.agregarElemento(20);
+// pila.agregarElemento(1000);
+// pila.mostrarElementos();
+
+class Cola{
+    constructor(){
+        this.cola=[];
+    }
+    agregarElementoCola(valor){
+        this.cola.push(valor);
+    }
+    eliminarElementoCola(){
+        this.cola.shift();
+    }
+    imprimir(){
+        console.log(this.cola)
+    }
+}
+let cola = new Cola();
+cola.agregarElementoCola(100);
+cola.agregarElementoCola(103);
+cola.agregarElementoCola(10220);
+cola.imprimir();
+
+function validarTarea(){
+    let tarea = document.getElementById("tarea").value;
+    console.log(tarea);
     let formato = /^[a-zA-Z]+\s[a-zA-Z]+$/;
     let nameFlag = 0;
-    if(name==""){
-        document.getElementById("errorName").style.display="block";
-        document.getElementById("errorName").innerHTML="Name is mandatory"
-    } else if(!(formato.test(name))){
-        document.getElementById("errorName").style.display="block";
-        document.getElementById("errorName").innerHTML="Invalid name characters"
+    if(tarea==""){
+        document.getElementById("errorTarea").style.display="block";
+        document.getElementById("errorTarea").innerHTML="Por favor ingrese una tarea"
+    } else if(!(formato.test(tarea))){
+        document.getElementById("errorTarea").style.display="block";
+        document.getElementById("errorTarea").innerHTML="Caracteres inválidos"
     }else {
-        document.getElementById("errorName").style.display="none";
+        document.getElementById("errorTarea").style.display="none";
         nameFlag = 1;
     }
+    console.log(nameFlag);
     return nameFlag;
 }
-
-function validatePhone(){
-    let phone = document.getElementById("phone").value;
-    let formato = /^3[0-9]{2}-[0-9]{4}-[0-9]{3}$/;
-    let phoneFlag = 0;
-    if(phone==""){
-        document.getElementById("errorPhone").style.display="block";
-        document.getElementById("errorPhone").innerHTML="Phone is mandatory"
-    } else if(!(formato.test(phone))){
-        document.getElementById("errorPhone").style.display="block";
-        document.getElementById("errorPhone").innerHTML="Invalid phone format"
-    }else {
-        document.getElementById("errorPhone").style.display="none";
-        phoneFlag = 1;
-    }
-    return phoneFlag;
-}
-function priceVehicle(){
-    let vehicle = document.getElementById("vehiculo").value;
-    let costVehicle = 0;
-    switch (vehicle){
-        case "automovil":
-            costVehicle = 100000;
-        break;
-        case "camioneta":
-            costVehicle = 200000;
-        break;
-        case "motocicleta":
-            costVehicle = 50000;
-        break;
-        case "microbus":
-            costVehicle = 500000;
-        break;
-    }
-    return costVehicle;
-}
-function towTruckValues(){
-    let severity = document.getElementById("severidad").value;
-    var towTruck = {
-        model: "Ford",
-        time: 0,
-        cost: 0,
-      };
-    switch (severity){
-        case "baja":
-            towTruck.cost = priceVehicle()*1.2;
-            towTruck.time = 90;
-        break;
-        case "media":
-            towTruck.cost = priceVehicle()*1.5;
-            towTruck.time = 60;
-        break;
-        case "urgente":
-            towTruck.cost = priceVehicle()*2;
-            towTruck.time = 30;
-        break;
-    }
-    return towTruck;
-}
-function truckAlert(){
-    const grua = towTruckValues();
-    window.alert(`Se enviará una grúa marca ${grua.model} en aproximadamente ${grua.time} minutos con un valor de $${grua.cost}`);
-    console.log(priceSeverity());
-}
-
-function validateFields(){
-    let nameFlag = validateName();
-    let phoneFlag = validatePhone();
-    if(nameFlag && phoneFlag){
-        truckAlert();
+function agregarElemento(){
+    if (validarTarea) {
+       cola.agregarElementoCola(tarea);
     }
 }
+function mostrarElemento(){
+    console.log("prueba");
+}
+
+document.getElementById("agregar").addEventListener("click", agregarElemento);
+document.getElementById("mostrar").addEventListener("click", mostrarElemento);
 
 
-document.getElementById("btnSend").addEventListener("click", validateFields);
